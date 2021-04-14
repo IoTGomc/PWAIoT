357dd9ec75b2bc336be2c75b63e985c8ca592279
IoTGomc <carlosgomc25@gmail.com>
2021-04-13 23:37:56 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ejemplo de una aplicación pwa con una funcionalidad de IoT
+
+
+https://alanypm.github.io/pwa-iot2/
---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCmoMchym3RDsP8FHVGlV015DCVHpu0uyA",
+    authDomain: "cagm-base-datos.firebaseapp.com",
+    databaseURL: "https://cagm-base-datos.firebaseio.com",
+    projectId: "cagm-base-datos",
+    storageBucket: "cagm-base-datos.appspot.com",
+    messagingSenderId: "999513027425",
+    appId: "1:999513027425:web:875b21934678ce23f9cb20"
+});
